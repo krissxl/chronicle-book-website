319d3e4101e215a4219fc729d9769ab30a62f8ef
Ilya Garenko <il.gareni4@gmail.com>
2020-08-17 15:51:02 +0300

    fix: fixed autoresize of textarea in 'add entry' page

--- src/app/app-page/entry/entry.component.ts
+++ src/app/app-page/entry/entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -7,21 +7,27 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./entry.component.scss'],
 })
 export class EntryComponent implements OnInit {
+  @ViewChild('invisible') invBlock: ElementRef;
   time: Date;
-  text: String = '';
+  text: string = '';
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       if (params.time) {
         this.time = new Date(+params.time);
       }
-    })
+    });
   }
 
-  inputTriger(event) {
-    const target = event.target as HTMLElement
-    target.style.height = target.scrollHeight + 'px';
+  resizeTrigger(event) {
+    const inv = this.invBlock.nativeElement as HTMLElement;
+    const target = event.target as HTMLElement;
+
+    inv.innerText = this.text;
+
+    console.log(this.text, inv.scrollHeight, target.scrollHeight);
+    target.style.height = inv.scrollHeight + 25 + 'px';
   }
 }
