ab595726158d46889f1d496252a6b0acf5e6d9a6
Ilya Garenko <il.gareni4@gmail.com>
2020-08-27 15:15:03 +0300

    upgraded calendar

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener,
+} from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { getFullDateName } from '../../scripts/date';
 
@@ -12,10 +19,47 @@ export class CalendarComponent implements OnInit {
   @Input() showOccupiedDays: boolean = true;
   days: number[];
   startWeekDay: number;
+  monthsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  yearSelectorArray: number[] = [];
+  isYearSelectorOpened: boolean = false;
+  isMonthSelectorOpened: boolean = false;
 
   @Output('dateChange') dateChange = new EventEmitter<Date>();
 
+  @HostListener('document:click', ['$event']) docClick(event: MouseEvent) {
+    const path = event.composedPath();
+    const yearSelector = document.querySelector('.year-selector');
+    const yearButton = document.querySelector('.year');
+
+    if (!path.includes(yearSelector) && !path.includes(yearButton))
+      this.isYearSelectorOpened = false;
+
+    const monthSelector = document.querySelector('.month-selector');
+    const monthButton = document.querySelector('.month');
+
+    if (!path.includes(monthSelector) && !path.includes(monthButton))
+      this.isMonthSelectorOpened = false;
+  }
+
   constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (!this.selectedDate) {
+      this.today();
+      this.dateChange.emit(this.selectedDate);
+    } else {
+      this.getMonthInfo();
+    }
+    this.setupYearSelector();
+  }
+
+  setupYearSelector() {
+    const now = new Date();
+    this.yearSelectorArray = [];
+    for (let i = now.getFullYear() - 70; i <= now.getFullYear() + 70; i++) {
+      this.yearSelectorArray.push(i);
+    }
+  }
 
   getMonthInfo(year?: number, month?: number): void {
     let time: Date;
@@ -102,6 +146,43 @@ export class CalendarComponent implements OnInit {
     this.dateChange.emit(this.selectedDate);
   }
 
+  selectYear(year: number): void {
+    this.selectedDate = new Date(
+      year,
+      this.selectedDate.getMonth(),
+      this.selectedDate.getDate()
+    );
+    this.isYearSelectorOpened = false;
+    this.getMonthInfo();
+
+    this.dateChange.emit(this.selectedDate);
+  }
+
+  selectMonth(month: number): void {
+    this.selectedDate = new Date(
+      this.selectedDate.getFullYear(),
+      month,
+      this.selectedDate.getDate()
+    );
+    this.isMonthSelectorOpened = false;
+    this.getMonthInfo();
+
+    this.dateChange.emit(this.selectedDate);
+  }
+
+  openCloseYearSelector() {
+    this.isYearSelectorOpened = !this.isYearSelectorOpened;
+
+    if (this.isYearSelectorOpened) {
+      const selectedElem = document.querySelector('.year-element.selected');
+      selectedElem.scrollIntoView();
+    }
+  }
+
+  getDateByMonth(month: number) {
+    return new Date(this.selectedDate.getFullYear(), month);
+  }
+
   today() {
     const now = new Date();
     this.selectedDate = new Date(
@@ -124,14 +205,5 @@ export class CalendarComponent implements OnInit {
       );
     }
     this.dateChange.emit(this.selectedDate);
-  }
-
-  ngOnInit(): void {
-    if (!this.selectedDate) {
-      this.today();
-      this.dateChange.emit(this.selectedDate);
-    } else {
-      this.getMonthInfo();
-    }
   }
 }
